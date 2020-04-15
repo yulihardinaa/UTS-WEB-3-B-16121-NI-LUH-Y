51d9f217d09ed9d785c674c6d86a4eebb0a03fcd
Yuli Hardina <yulihardinap@gmail.com>
2020-04-15 16:08:09 +0800

    Updates

---
+++ precache-manifest.40247a7b6be1454ab784ec8a2c765249.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05fb88f4d9b3d0a967f55e59517f3d16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db6e490ac14ff982e3e7",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "7a1afe92caba3e80b40b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "db6e490ac14ff982e3e7",
+    "url": "/static/js/2.ab7d8b43.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ab7d8b43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a1afe92caba3e80b40b",
+    "url": "/static/js/main.7885b655.chunk.js"
+  },
+  {
+    "revision": "8386b88ebb74532e3082",
+    "url": "/static/js/runtime-main.f81855d0.js"
+  }
+]);
